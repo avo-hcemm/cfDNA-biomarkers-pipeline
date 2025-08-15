958fc9a4d3d4daa60bd83748783cc6d306afd76b
avo <alessandra.vittorini@hcemm.eu>
2025-08-15 13:21:41 +0200

    basic_mod2to6_tool tested OK (missing python folder)+run_pipeline_module2to6.sh tested OK

--- galaxy/run_pipeline_module2to6.sh
+++ galaxy/run_pipeline_module2to6.sh
@@ -10,26 +10,46 @@ SPECIES=""
 CHROMOSOME_JOB=false
 CHR=""
 OUTPUTDIR=""
-INPUTDIR=""
 
 echo "Raw arguments: $@"
 echo "number raw arguments: $#"
 
-INPUTDIR="$2"
-OUTPUTDIR="$4"
-GENOMEINFO="$5"
-PARAMETERSINFO="$6"
-DATAINFO="$7"
-SPECIES="$8"
-CHR="${10}"
+# Trim function
+trim() {
+    local var="$*"
+    # remove leading/trailing whitespace
+    var="${var#"${var%%[![:space:]]*}"}"
+    var="${var%"${var##*[![:space:]]}"}"
+    echo -n "$var"
+}
 
-if [[ $CHR != "" ]]; then
+# Apply trim
+OUTPUTDIR="$(trim "$2")"
+GENOMEINFO="$(trim "$3")"
+PARAMETERSINFO="$(trim "$4")"
+
+if [[ $# -eq 5 ]]; then
+    SPECIES="$(trim "$5")"
+elif [[ $# -eq 6 ]]; then
+    DATAINFO="$(trim "$5")"
+    SPECIES="$(trim "$6")"
+elif [[ $# -eq 7 ]]; then
+    SPECIES="$(trim "$5")"
+    CHR="$(trim "$7")"
+elif [[ $# -eq 8 ]]; then
+    DATAINFO="$(trim "$5")"
+    SPECIES="$(trim "$6")"
+    CHR="$(trim "$8")"
+fi
+
+
+if [[ -n "$CHR" ]]; then
   CHROMOSOME_JOB=true
 fi
 
 # Validation
-if [[ -z "$GENOMEINFO" || -z "$PARAMETERSINFO" || -z "$SPECIES" ]]; then
-    echo "Usage: $0 -i <INPUTDIR> -o <OUTPUTDIR> <GENOMEINFO.csv> <PARAMETERSINFO.csv> [DATAINFO.csv] <SPECIES> [--chromosome CHR]"
+if [[ -z "$OUTPUTDIR"  || -z "$GENOMEINFO" || -z "$PARAMETERSINFO" || -z "$SPECIES" ]]; then
+    echo "Usage: $0 -o <OUTPUTDIR> <GENOMEINFO.csv> <PARAMETERSINFO.csv> [DATAINFO.csv] <SPECIES> [--chromosome CHR]"
     exit 1
 fi
 
@@ -37,13 +57,12 @@ echo "Current directory before 'cd' command:"
 ls -lh 
 
 echo "Running Java with arguments:"
-echo "GENOMEINFO: ${GENOMEINFO}"
-echo "PARAMETERSINFO: $PARAMETERSINFO"
-echo "DATAINFO: $DATAINFO"
-echo "SPECIES: $SPECIES"
-echo "INPUT DIR: $INPUTDIR"
-echo "OUTPUT DIR: $OUTPUTDIR"
-echo "CHROMOSOME JOB: $CHR"
+echo "GENOMEINFO:${GENOMEINFO}"
+echo "PARAMETERSINFO:$PARAMETERSINFO"
+echo "DATAINFO:$DATAINFO"
+echo "SPECIES:$SPECIES"
+echo "OUTPUT DIR:$OUTPUTDIR"
+echo "CHROMOSOME JOB:$CHR"
 [[ "$CHROMOSOME_JOB" == "true" ]] && echo "Chromosome: $CHR"
 
 if [[ -z "$OUTPUTDIR" ]]; then
@@ -52,12 +71,14 @@ if [[ -z "$OUTPUTDIR" ]]; then
 fi
 
 cd "$OUTPUTDIR" || { echo "Error: Failed to change to $OUTPUTDIR"; exit 1; }
-echo "Genome directory after 'cd' command:"
-ls -lh src/genome/human/hg38
 echo "Current directory after 'cd' command:"
 ls -lh
-# echo "Show genome file content:"
-# head input/genome.csv
+echo "Input directory after 'cd' command:"
+ls -lh input/*
+echo "Samples directory after 'cd' command:"
+ls -lh samples/*
+echo "Show genome file content:"
+head input/$SPECIES/genome.csv
 # echo "Now in: $PWD"
 
 # echo "check the actual file type"
@@ -66,7 +87,7 @@ ls -lh
 
 # Auxiliary parameters to run the jar file
 HEAP_SIZE=${JAVA_HEAP:-32g}
-JAR_PATH=${JAR_PATH:-jars/jcna-kldiv_16.3.jar}
+JAR_PATH=${JAR_PATH:-jars/jtool.jar}
 LOGFILE_PATH=${LOGFILE_PATH:-config/log4j2.xml}
 echo "Java command: java -Xmx$HEAP_SIZE -Dlog4j.configurationFile=$LOGFILE_PATH -jar $JAR_PATH ..."
 
@@ -74,33 +95,33 @@ echo "Java command: java -Xmx$HEAP_SIZE -Dlog4j.configurationFile=$LOGFILE_PATH 
 if [[ -n "$DATAINFO" && "$CHROMOSOME_JOB" == "true" ]]; then
 echo "Running with data file & chromosome for the job"
   java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    $JAR_PATH\
-    $GENOMEINFO \
-    $PARAMETERSINFO \
-    $DATAINFO \
-    $SPECIES \
+    "$JAR_PATH" \
+    "$GENOMEINFO" \
+    "$PARAMETERSINFO" \
+    "$DATAINFO" \
+    "$SPECIES" \
     --chromosome "$CHR"
 elif [[ -n "$DATAINFO" && "$CHROMOSOME_JOB" == "false" ]]; then
 echo "Running with data file & no chromosome for the job"
   java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    $JAR_PATH \
-    $GENOMEINFO \
-    $PARAMETERSINFO \
-    $DATAINFO \
-    $SPECIES 
+    "$JAR_PATH" \
+    "$GENOMEINFO" \
+    "$PARAMETERSINFO" \
+    "$DATAINFO" \
+    "$SPECIES" 
 elif [[ "$CHROMOSOME_JOB" == "true" ]]; then
 echo "Running with no data file & chromosome for the job"
   java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    $JAR_PATH \
-    $GENOMEINFO \
-    $PARAMETERSINFO \
-    $SPECIES \
+    "$JAR_PATH" \
+    "$GENOMEINFO" \
+    "$PARAMETERSINFO" \
+    "$SPECIES" \
     --chromosome "$CHR"
 else
 echo "Running with no data file & no chromosome for the job"
   java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    $JAR_PATH \
-    $GENOMEINFO \
-    $PARAMETERSINFO \
-    $SPECIES
+    "$JAR_PATH" \
+    "$GENOMEINFO" \
+    "$PARAMETERSINFO" \
+    "$SPECIES"
 fi
