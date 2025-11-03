ca41b74a3ea15f162a573db3757becf0014e7aac
avo <alessandra.vittorini@hcemm.eu>
2025-11-03 17:40:58 +0100

    correct version of docker image that contains java-jdk 21, trimmomatic, bowtie2, samtools, fastqc, multiqc

--- galaxy/run_pipeline_module2to6.sh
+++ galaxy/run_pipeline_module2to6.sh
@@ -81,22 +81,22 @@ ls -lh
 # head input/$SPECIES/genome.csv
 # echo "Now in: $PWD"
 
-CPUS_PER_TASK=${GALAXY_SLOTS:-50}
-TOTAL_MEM_MB=${GALAXY_MEMORY_MB:-64000}
+CPUS_PER_TASK=${GALAXY_SLOTS:-1}
+TOTAL_MEM_MB=${GALAXY_MEMORY_MB:-32000}
 MEM_PER_CPU_MB=$(( TOTAL_MEM_MB / CPUS_PER_TASK ))
 HEAP_MEM_GB=$(( TOTAL_MEM_MB * 9 / 10240 ))  # 9/10 of total memory in GB
 
 # Auxiliary parameters to run the jar file
+# Define CLASSPATH if not already set in the environment
+CLASSPATH=${CLASSPATH:-/app/jars/jcna-biomrkrs-v11.2.3.jar:/app/libs/all-libraries.jar:/app/libs/zstd-jni-1.5.7-4-linux_amd64.jar}
 HEAP_SIZE=${HEAP_MEM_GB:-32}G
-JAR_PATH=${JAR_PATH:-jars/jtool.jar}
-LOGFILE_PATH=${LOGFILE_PATH:-config/log4j2.xml}
-echo "Java command: java -Xmx$HEAP_SIZE -Dlog4j.configurationFile=$LOGFILE_PATH -jar $JAR_PATH ..."
+LOGFILE_PATH=${LOGFILE_PATH:-/app/config/log4j2.xml}
+echo "Java command: java -Xmx$HEAP_SIZE -Dlog4j.configurationFile=$LOGFILE_PATH -cp "$CLASSPATH" task.test.Jcna_input ..."
 
 # Run Java analysis with or without chromosome and DATAINFO
 if [[ -n "$DATAINFO" && "$CHROMOSOME_JOB" == "true" ]]; then
 echo "Running with data file & chromosome for the job"
-  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    "$JAR_PATH" \
+  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -cp "$CLASSPATH" task.test.Jcna_input \
     "$GENOMEINFO" \
     "$PARAMETERSINFO" \
     "$DATAINFO" \
@@ -104,24 +104,21 @@ echo "Running with data file & chromosome for the job"
     --chromosome "$CHR"
 elif [[ -n "$DATAINFO" && "$CHROMOSOME_JOB" == "false" ]]; then
 echo "Running with data file & no chromosome for the job"
-  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    "$JAR_PATH" \
+  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -cp "$CLASSPATH" task.test.Jcna_input \
     "$GENOMEINFO" \
     "$PARAMETERSINFO" \
     "$DATAINFO" \
     "$SPECIES" 
 elif [[ "$CHROMOSOME_JOB" == "true" ]]; then
 echo "Running with no data file & chromosome for the job"
-  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    "$JAR_PATH" \
+  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -cp "$CLASSPATH" task.test.Jcna_input \
     "$GENOMEINFO" \
     "$PARAMETERSINFO" \
     "$SPECIES" \
     --chromosome "$CHR"
 else
 echo "Running with no data file & no chromosome for the job"
-  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -jar \
-    "$JAR_PATH" \
+  java -Xmx"$HEAP_SIZE" -Dlog4j.configurationFile="$LOGFILE_PATH" -cp "$CLASSPATH" task.test.Jcna_input \
     "$GENOMEINFO" \
     "$PARAMETERSINFO" \
     "$SPECIES"
